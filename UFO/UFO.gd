cf0a0f17ff1b56e36b6bba0aa54eb21750b2d56f
Met48 <metroid48@gmail.com>
2018-10-03 02:00:18 +0000

    Destroy UFOs when hit by bullet, spawn explosion

--- UFO/UFO.gd
+++ UFO/UFO.gd
@@ -4,9 +4,16 @@ extends KinematicBody2D
 export var speed = 600
 
 
+onready var Explosion = load("res://Explosion/Explosion.tscn")
+
+
 var path_curve = null
 var path_offset = null
 var path_pos = 0.0
+
+
+func _ready():
+	visible = false
 
 
 func set_path(new_path):
@@ -19,6 +26,7 @@ func set_path(new_path):
 func _physics_process(delta):
 	if not path_curve:
 		return
+	visible = true
 	path_pos += delta * speed
 	var curve_length = path_curve.get_baked_length()
 	if path_pos > curve_length:
@@ -27,3 +35,9 @@ func _physics_process(delta):
 	else:
 		global_position = path_offset + path_curve.interpolate_baked(path_pos)
 
+
+func receive_damage(source):
+	var explosion = Explosion.instance()
+	explosion.position = position
+	get_parent().add_child(explosion)
+	queue_free()
